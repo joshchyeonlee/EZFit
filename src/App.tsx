dd086252a73f0cb5a2b823d965bf85c7f813580b
abenavidesaguirre <61483968+abenavidesaguirre@users.noreply.github.com>
2023-10-24 20:18:47 -0600

    Create App + MUI Theming

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div className="App">
+      EZFIT
+      <Button color="primary" variant="outlined">
+        Primary
+      </Button>
+      <Button color="secondary" variant="outlined">
+        Secondary
+      </Button>
+      <Button color="error" variant="outlined">
+        Error
+      </Button>
+      <Button color="warning" variant="outlined">
+        Warning
+      </Button>
+      <Button color="info" variant="outlined">
+        Info
+      </Button>
+    </div>
+  );
+}
+
+export default App;
