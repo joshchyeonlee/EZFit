15f5700732ef97568b6b2fc115b9d7f6787f064d
joshchyeonlee <joshchyeonlee@gmail.com>
2023-10-26 16:44:53 -0600

    remove accountconnectdevice from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      {/* <App /> */}
-      <AccountConnectDevice/>
+      <App />
+      {/* <AccountConnectDevice/> */}
     </ThemeProvider>
   </React.StrictMode>
 );
