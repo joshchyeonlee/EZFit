e7d609cfbc14d6ef82e1c708b30746ed0d9dab32
E-man248 <67338595+E-man248@users.noreply.github.com>
2023-11-20 12:12:45 -0700

    Added Alert Notification (WIP)

--- src/components/account/PersonalInformation/DeleteAccountPopUp.tsx
+++ src/components/account/PersonalInformation/DeleteAccountPopUp.tsx
@@ -1,7 +1,12 @@
 import { Close } from "@mui/icons-material";
-import { Box, Button, IconButton, Modal, Typography } from "@mui/material";
+import { Alert, Box, Button, IconButton, Modal, Snackbar, Typography } from "@mui/material";
+import { useState } from "react";
 
 function DeleteAccountPopUp(props : {open : boolean, setOpen : any, onAccountDeleted : any}) {
+
+    const [showAccountDeleted, setShowAccountDeleted] = useState(false);
+
+    const deleteText = "Account Deleted!";
 
     const handleClose = (accountDeleted : boolean) =>
     {
@@ -28,37 +33,49 @@ function DeleteAccountPopUp(props : {open : boolean, setOpen : any, onAccountDel
     };
 
     return (
-        <Modal open={props.open}>
-             <Box sx={style} justifyContent="center">
-                <Box id="Close" position="absolute" display="flex" justifyContent="flex-end" right="10px" top="10px" padding="10px">
-                    <IconButton onClick={() => handleClose(false)}><Close/></IconButton>
-                </Box>
-                <Box textAlign="center" justifyContent="center" width="90%">
-                    <Box id="Title" textAlign="center" justifyContent="center" paddingBottom={4}>
-                        <Typography id="Title" variant="h5" sx={{ fontWeight: 'bold'}}>
-                            Delete Account
-                        </Typography>
+        <Box>
+            <Snackbar
+                open={showAccountDeleted}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                autoHideDuration={10000}
+                onClose={() => setShowAccountDeleted(false)}
+            >
+                <Alert onClose={() => setShowAccountDeleted(false)}>
+                    {deleteText}
+                </Alert>
+            </Snackbar>
+            <Modal open={props.open}>
+                <Box sx={style} justifyContent="center">
+                    <Box id="Close" position="absolute" display="flex" justifyContent="flex-end" right="10px" top="10px" padding="10px">
+                        <IconButton onClick={() => handleClose(false)}><Close/></IconButton>
                     </Box>
-                    <Box id="Content" textAlign="center" justifyContent="center" height="50%">
-                        <Box id="Content" textAlign="center" justifyContent="start" paddingTop="15%" paddingBottom="20%">
-                            <Typography sx={{fontSize:"18px"}}>
-                                Deleting your account is irreversible, are you sure you want to proceed?
+                    <Box textAlign="center" justifyContent="center" width="90%">
+                        <Box id="Title" textAlign="center" justifyContent="center" paddingBottom={4}>
+                            <Typography id="Title" variant="h5" sx={{ fontWeight: 'bold'}}>
+                                Delete Account
                             </Typography>
                         </Box>
+                        <Box id="Content" textAlign="center" justifyContent="center" height="50%">
+                            <Box id="Content" textAlign="center" justifyContent="start" paddingTop="15%" paddingBottom="20%">
+                                <Typography sx={{fontSize:"18px"}}>
+                                    Deleting your account is irreversible, are you sure you want to proceed?
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Button fullWidth size="large" variant="contained" color="error" onClick={ () => {handleClose(true)} } sx={{marginBottom:2}} disableElevation>
+                            <Typography color="white">
+                                Delete Account
+                            </Typography>
+                        </Button>
+                        <Button fullWidth size="large" variant="contained" color="secondary" onClick={ () => {handleClose(false)} } sx={{marginBottom:2, background:"grey"}} disableElevation>
+                            <Typography color="white">
+                                Cancel
+                            </Typography>
+                        </Button>
                     </Box>
-                    <Button fullWidth size="large" variant="contained" color="error" onClick={ () => {handleClose(true)} } sx={{marginBottom:2}} disableElevation>
-                        <Typography color="white">
-                            Delete Account
-                        </Typography>
-                    </Button>
-                    <Button fullWidth size="large" variant="contained" color="secondary" onClick={ () => {handleClose(false)} } sx={{marginBottom:2, background:"grey"}} disableElevation>
-                        <Typography color="white">
-                            Cancel
-                        </Typography>
-                    </Button>
                 </Box>
-            </Box>
-        </Modal>
+            </Modal>
+        </Box>
     );
 }
   
