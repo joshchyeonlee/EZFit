87592a48990c9864b62af3b16f6be851696e264d
joshchyeonlee <63981671+joshchyeonlee@users.noreply.github.com>
2023-11-20 19:03:36 -0700

    add styling and pagination

--- src/components/dashboard/AddWidgetModal.tsx
+++ src/components/dashboard/AddWidgetModal.tsx
@@ -1,16 +1,27 @@
 import { Box, Button, IconButton, Modal, Typography, Grid, Card, CardActionArea } from "@mui/material";
 import { Close } from '@mui/icons-material';
 import { ChevronLeft, ChevronRight } from '@mui/icons-material';
+import { useState } from "react";
+import theme from "../../themes/theme";
 
 function AddWidgetModal(props: { open: boolean; setOpen: any; }){
-    const availableModals = ["Active Minutes", "Today's Macros", "Distance Travelled", "Heart Rate"];
+    const availableModals = ["Active Minutes", "Today's Macros", "Distance Travelled", "Heart Rate", "Steps"];
+    const [index, setIndex] = useState(0);
+    const [selectedModal, setSelectedModal] = useState("");
 
     const handleClose = () => {
+        setSelectedModal("");
         props.setOpen(false);
     }
 
     const handleChevronClick = (i: number) => {
-        console.log("hehe");
+        const currentIndex = (i === 1) ? index + 4 : index - 4;
+        setIndex(currentIndex);
+    }
+
+    const handleSelectModal = (modal: string) => {
+        console.log(`clicked ${modal}`);
+        setSelectedModal(modal);
     }
 
     const style = {
@@ -23,7 +34,6 @@ function AddWidgetModal(props: { open: boolean; setOpen: any; }){
         borderRadius: "50px",
         boxShadow: 24,
         p: 4,
-        display: 'flex',
     };
 
     return(
@@ -33,24 +43,31 @@ function AddWidgetModal(props: { open: boolean; setOpen: any; }){
         aria-labelledby="add-widget"
         aria-describedby="add-widget-modal"
     >
-        <Box sx={style} justifyContent="center">
+        <Box sx={style} justifyContent="center" width={700} height={500}>
             <Box position="absolute" display="flex" justifyContent="flex-end" right="10px" top="10px" padding="10px">
                 <IconButton size="large" onClick={() => handleClose()}><Close/></IconButton>
             </Box>
             <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center">
                 <Typography variant="h5"  sx={{ fontWeight: 'bold' }} >Add Widget</Typography>
                 <Box display="flex" justifyContent="center" paddingTop={2}>
-                    <IconButton onClick={() => handleChevronClick(-1)} disabled>
+                    <IconButton onClick={() => handleChevronClick(-1)} disabled={index - 4 < 0}>
                         <ChevronLeft/>
                     </IconButton>
-                    <Box padding={4}>    
+                    <Box padding={4} display="absolute" width={550} height={300}>    
                         <Grid container spacing={2}>
-                            {availableModals.map((val) => {return(
+                            {availableModals.slice(index, index+4).map((val) => {return(
                             <Grid item xs={6}>
-                                <Card variant="outlined" sx={{ bgcolor: "ButtonShadow", borderRadius: 4, boxShadow: 3, minHeight: 150, minWidth: 250 }}>
-                                    <CardActionArea sx={{ minHeight: 150, minWidth: 250}}>
+                                <Card variant="outlined"
+                                        sx={{   bgcolor: (val === selectedModal) ? theme.palette.primary.main : "ButtonShadow",
+                                                borderRadius: 4,
+                                                boxShadow: 3,
+                                                minHeight: 150,
+                                                minWidth: 250,
+                                                transition: "all .5s ease",
+                                                }}>
+                                    <CardActionArea sx={{ minHeight: 150, minWidth: 250}} onClick={() => {handleSelectModal(val)}}>
                                         <Box display="flex" justifyContent="center">
-                                            <Typography variant="h5">{val}</Typography>
+                                            <Typography variant="h5" color={(val === selectedModal) ? "white" : "black"}>{val}</Typography>
                                         </Box>
                                     </CardActionArea>
                                 </Card>
@@ -58,11 +75,11 @@ function AddWidgetModal(props: { open: boolean; setOpen: any; }){
                             )})}
                         </Grid>
                     </Box>
-                    <IconButton size="large" onClick={() => handleChevronClick(-1)} disabled>
+                    <IconButton size="large" onClick={() => handleChevronClick(1)} disabled={index + 4 >= availableModals.length}>
                         <ChevronRight/>
                     </IconButton>
                 </Box>
-                <Box padding={2}>
+                <Box padding={4}>
                     <Button variant="contained">
                         Confirm
                     </Button>
