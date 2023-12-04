4c1dbff9095923b8a4831d634aa322f6e02d608d
HaziqKhawaja <93850451+HaziqKhawaja@users.noreply.github.com>
2023-12-03 23:38:24 -0700

    Implemented most error handling for sign up

--- src/components/signup/SignUp.tsx
+++ src/components/signup/SignUp.tsx
@@ -1,29 +1,78 @@
 import { Button, Box, Typography, TextField, IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
 interface SignUpButtonProps {
   title: string;
   onClick?: () => void;
 }
 
-const SignUpButton = ({ title, onClick }: SignUpButtonProps) => {
-  return (
-    <Box textAlign={"center"} width={"40%"} padding={"0px 8px"}>
-      <Button
-        variant="contained"
-        color="primary"
-        fullWidth
-        sx={{ minHeight: "28px" }}
-        onClick={onClick}
-      >
-        <Typography color={"primaryBkg"}>{title}</Typography>
-      </Button>
-    </Box>
-  );
-};
-
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isFirstNameValid, setIsFirstNameValid] = useState(false);
+  const [isLastNameValid, setIsLastNameValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState(false);
+  const [isPasswordMatch, setIsPasswordMatch] = useState(false);
+
+  const validateEmail = () => {
+    const emailRegex = /^([a-zA-Z0-9-.]+)@([a-zA-Z0-9-.]+)\.([a-zA-Z]{1,5})$/;
+    const isValid = emailRegex.test(email);
+    setIsEmailValid(isValid);
+    return isValid;
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    validateEmail();
+  };
+
+  const validatePasswordMatch = () => {
+    const state = password === confirmPassword ? true : false;
+    console.log(`password: ${password}`);
+    console.log(`confirmPassword: ${confirmPassword}`);
+    console.log(`State: ${state}`);
+    setIsPasswordMatch(state);
+    return state;
+  };
+
+  const isFormValid = () => {
+    return (
+      isEmailValid &&
+      isFirstNameValid &&
+      isLastNameValid &&
+      isPasswordValid &&
+      isConfirmPasswordValid
+      // &&
+      // isPasswordMatch
+    );
+  };
+
+  const SignUpButton = ({ title, onClick }: SignUpButtonProps) => {
+    return (
+      <Box textAlign={"center"} width={"40%"} padding={"0px 8px"}>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ minHeight: "28px" }}
+          onClick={onClick}
+          disabled={!isFormValid()}
+          type="submit"
+        >
+          <Typography color={"primaryBkg"}>{title}</Typography>
+        </Button>
+      </Box>
+    );
+  };
+
   const navigateSignUpClick = useNavigate();
 
   const handleSignUpClick = () => {
@@ -45,7 +94,7 @@ function SignUp() {
     >
       <Box maxWidth={"300px"} minWidth={"200px"}>
         <img
-          src="/EZFitLogo.png"
+          src="EZFitLogo.png"
           alt="EZFit Logo"
           width={"100%"}
           height={"auto"}
@@ -56,7 +105,7 @@ function SignUp() {
           <ArrowBackIcon color="primary" />
         </IconButton>
       </Box>
-      <Typography variant="h5" sx={{ mt: 7 }}>
+      <Typography variant="h5" textAlign="center" sx={{ mt: 7 }}>
         Sign Up
       </Typography>
       <Box
@@ -66,7 +115,13 @@ function SignUp() {
         sx={{ mt: 3 }}
       >
         <Typography>Email Address</Typography>
-        <TextField size="small"></TextField>
+        <TextField
+          size="small"
+          required
+          error={!isEmailValid}
+          value={email}
+          onChange={handleEmailChange}
+        ></TextField>
       </Box>
       <Box
         display={"flex"}
@@ -76,7 +131,15 @@ function SignUp() {
         sx={{ mt: 2 }}
       >
         <Typography>First Name</Typography>
-        <TextField size="small"></TextField>
+        <TextField
+          size="small"
+          required
+          error={!isFirstNameValid}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+            setIsFirstNameValid(e.target.value !== "");
+          }}
+        ></TextField>
       </Box>
       <Box
         display={"flex"}
@@ -86,7 +149,15 @@ function SignUp() {
         sx={{ mt: 2 }}
       >
         <Typography>Last Name</Typography>
-        <TextField size="small"></TextField>
+        <TextField
+          size="small"
+          required
+          error={!isLastNameValid}
+          onChange={(e) => {
+            setLastName(e.target.value);
+            setIsLastNameValid(e.target.value !== "");
+          }}
+        ></TextField>
       </Box>
       <Box
         display={"flex"}
@@ -96,7 +167,17 @@ function SignUp() {
         sx={{ mt: 2 }}
       >
         <Typography>Password</Typography>
-        <TextField size="small"></TextField>
+        <TextField
+          size="small"
+          required
+          error={!isPasswordValid}
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+            setIsPasswordValid(e.target.value !== "");
+            // validatePasswordMatch();
+          }}
+        ></TextField>
       </Box>
       <Box
         display={"flex"}
@@ -106,7 +187,17 @@ function SignUp() {
         sx={{ mt: 2 }}
       >
         <Typography>Confirm Password</Typography>
-        <TextField size="small"></TextField>
+        <TextField
+          size="small"
+          required
+          error={!isConfirmPasswordValid}
+          type="password"
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+            setIsConfirmPasswordValid(e.target.value !== "");
+            // validatePasswordMatch();
+          }}
+        ></TextField>
       </Box>
       <Box sx={{ mt: 4 }}></Box>
       <SignUpButton title={"Sign Up"} onClick={handleSignUpClick} />
